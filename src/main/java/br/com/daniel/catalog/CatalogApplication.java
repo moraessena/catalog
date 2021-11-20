4d077ce3bb53831bfbb91db9294f85529d3ab771
daniel <danielsena04@gmail.com>
2021-11-20 16:25:07 -0300

    config: setup service discovery client

--- src/main/java/br/com/daniel/catalog/CatalogApplication.java
+++ src/main/java/br/com/daniel/catalog/CatalogApplication.java
@@ -2,8 +2,10 @@ package br.com.daniel.catalog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CatalogApplication {
 
 	public static void main(String[] args) {
